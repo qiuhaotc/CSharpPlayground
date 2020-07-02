3c28ada4839e2dacc61825de2bbba4899731fbfc
QH-PC-NEW\qiuhao <qiu66hao@163.com>
2020-07-02 22:37:26 +0800

    Initialize

---
+++ src/CSharpPlayground/CrossPlatformApp/CrossPlatformApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CrossPlatformApp.Services;
+using CrossPlatformApp.Views;
+
+namespace CrossPlatformApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/CSharpPlayground/LearnReactiveUI/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+using Splat;
+
+namespace LearnReactiveUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // A helper method that will register all classes that derive off IViewFor 
+            // into our dependency injection container. ReactiveUI uses Splat for it's 
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+        }
+    }
+}
