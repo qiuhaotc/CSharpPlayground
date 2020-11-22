7d8acfd3fc6f6d7574d9275888b76edd2b6b04dc
qiuhaotc <qiu66hao@163.com>
2020-11-22 09:43:11 +0800

    done

--- src/CSharpPlayground/Try.Net50/Program.cs
+++ src/CSharpPlayground/Try.Net50/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Try.Net50
 {
@@ -26,6 +27,8 @@ namespace Try.Net50
             var wow2 = wow with { BBB = "New B" };
             Console.WriteLine("vwow.ToString() " + wow.ToString());
             Console.WriteLine("var wow2 = wow with { BBB = \"New B\" }; wow2.ToString() " + wow2.ToString());
+
+            Console.WriteLine("subRecord.GetType().IsAssignableTo(typeof(DummyRecord)) " + subRecord.GetType().IsAssignableTo(typeof(DummyRecord)));
 
             #endregion Record types
 
@@ -61,18 +64,56 @@ namespace Try.Net50
                 Console.WriteLine("e is null");
             }
 
+            foreach (var item in new object[] { "ABC", 123, new Person { Name = "HAHA" }, new Person { Name = "WOWO" }, new Person { Name = "Somebody else" } })
+            {
+                switch (item)
+                {
+                    case string _:
+                        Console.WriteLine("e is string");
+                        break;
+
+                    case int intE:
+                        Console.WriteLine("e is int " + intE);
+                        break;
+
+                    case Person personE when personE.Name == "HAHA":
+                        Console.WriteLine("e is Person " + personE.Name);
+                        break;
+
+                    case Person personD when personD.Name == "WOWO":
+                        Console.WriteLine("e is Person " + personD.Name);
+                        break;
+
+                    case Person _:
+                        Console.WriteLine("e is Person and not named as HAHA or WOWO");
+                        break;
+                }
+            }
+
             #endregion Pattern matching enhancements
 
-            // To be continue...
+            #region Fit and finish features
+
+            NewSection("Fit and finish features");
+
+            List<int> list = new();
+
+            Console.WriteLine("List<int> list = new(); " + (list is not null));
+
+            HooHooHoo hoo = new("Hoo");
+
+            Console.WriteLine("HooHooHoo hoo = new(\"Hoo\"); " + (hoo is not null));
+
+            hoo.SayHiToPerson(new() { Name = "Mr. Curious" });
+
+            #endregion Fit and finish features
         }
 
         static bool IsLetter(char c) => c is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
 
         static void NewSection(string name)
         {
-            Console.WriteLine();
-            Console.WriteLine("/**********************************************************/");
-            Console.WriteLine(name);
+            Console.WriteLine(Environment.NewLine + $"/******************** {name} ********************/");
             Console.WriteLine();
         }
     }
@@ -104,5 +145,20 @@ namespace Try.Net50
     public struct Person
     {
         public string Name { get; init; }
+    }
+
+    class HooHooHoo
+    {
+        public HooHooHoo(string hoohoo)
+        {
+            Hoohoo = hoohoo;
+        }
+
+        public string Hoohoo { get; }
+
+        public void SayHiToPerson(Person person)
+        {
+            Console.WriteLine("Hi " + person.Name);
+        }
     }
 }
